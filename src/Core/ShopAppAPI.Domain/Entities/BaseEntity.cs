efa4864582cb1cabe68e937c78910a8ed59e0cc6
can33 <cankecef@hotmail.com>
2023-09-17 13:26:59 +0300

    Base and category entities created.

---
+++ src/Core/ShopAppAPI.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopAppAPI.Domain;
+
+public class BaseEntity
+{
+    //public Guid Id { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
+    public StatusTypeEnum Status { get; set; }
+}
---
+++ src/Core/ShopAppAPI.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopAppAPI.Domain.Entities;
+
+public class Category : BaseEntity
+{
+    public Category()
+    {
+        Products = new List<Product>();
+    }
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public ICollection<Product> Products { get; set; }
+}
