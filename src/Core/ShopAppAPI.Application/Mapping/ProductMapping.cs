1fdf16b869a97a760b3ae78e081bc7b390b796fc
can33 <cankecef@hotmail.com>
2023-09-17 13:32:38 +0300

    Product mapping added for automapper.

---
+++ src/Core/ShopAppAPI.Application/Mapping/ProductMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ShopAppAPI.Application.Features.ProductCommandQuery.Commands;
+using ShopAppAPI.Domain.Entities;
+
+namespace ShopAppAPI.Application.Mapping;
+
+public class ProductMapping : Profile
+{
+    public ProductMapping()
+    {
+        CreateMap<Product, GetAllProductQueryResponse>().ReverseMap();
+        CreateMap<Product, CreateProductCommandRequest>().ReverseMap();
+    }
+}
