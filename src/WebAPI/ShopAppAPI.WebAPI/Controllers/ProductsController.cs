1f521c4c3873b6b75901815f03c38beeb94b5972
can33 <cankecef@hotmail.com>
2023-09-17 13:34:42 +0300

    Product controller added.

---
+++ src/WebAPI/ShopAppAPI.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ShopAppAPI.Application.Features.ProductCommandQuery.Commands;
+using ShopAppAPI.Application.Features.ProductCommandQuery.Queries.GetAllProducts;
+
+namespace ShopAppAPI.WebAPI.Controllers;
+
+[ApiController, Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProductsController(IMediator mediator) => _mediator = mediator;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+        => Ok(await _mediator.Send(new GetAllProductsQueryRequest()));
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateProductCommandRequest request)
+    {
+        var response = await _mediator.Send(request);
+        return StatusCode(201, response);
+    }
+
+}
--- src/WebAPI/ShopAppAPI.WebAPI/Program.cs
+++ src/WebAPI/ShopAppAPI.WebAPI/Program.cs
@@ -1,7 +1,12 @@
+using ShopAppAPI.Persistance;
+using ShopAppAPI.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddPersistanceRegistration(builder.Configuration);
+builder.Services.AddApplicationRegistration();
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
