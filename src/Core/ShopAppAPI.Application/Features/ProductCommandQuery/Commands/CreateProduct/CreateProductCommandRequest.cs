af201e64c2d422fb0441446ccef17e6b3aa4af94
can33 <cankecef@hotmail.com>
2023-10-01 10:29:52 +0300

    Product create service refactored.

---
+++ src/Core/ShopAppAPI.Application/Features/ProductCommandQuery/Commands/CreateProduct/CreateProductCommandRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using ShopAppAPI.Domain;
+
+namespace ShopAppAPI.Application.Features.ProductCommandQuery.Commands.CreateProduct;
+
+public class CreateProductCommandRequest : IRequest<BaseResponse<CreateProductCommandResponse>>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+    public StatusTypeEnum StatusType { get; set; } = StatusTypeEnum.Active;
+}
---
+++ src/Core/ShopAppAPI.Application/Features/ProductCommandQuery/Commands/CreateProduct/CreateProductCommandResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace ShopAppAPI.Application.Features.ProductCommandQuery.Commands.CreateProduct;
+
+public class CreateProductCommandResponse
+{
+    public Guid Id { get; set; }
+}
