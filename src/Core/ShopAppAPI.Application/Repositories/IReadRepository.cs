2bde40c78d16a1d4b2c7e081bf954a6566e5bf0f
can33 <cankecef@hotmail.com>
2023-11-11 22:42:56 +0300

    Read repository changed.

--- src/Core/ShopAppAPI.Application/Repositories/IReadRepository.cs
+++ src/Core/ShopAppAPI.Application/Repositories/IReadRepository.cs
@@ -1,14 +1,11 @@
-﻿using ShopAppAPI.Domain;
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 
-namespace ShopAppAPI.Application.Repositories
+namespace ShopAppAPI.Application.Repositories;
+
+public interface IReadRepository<T> : IRepository<T> where T : class, new()
 {
-    public interface IReadRepository<T> : IRepository<T> where T : class, new()
-    {
-        Task<List<T>> GetAllAsync(int page = 1, int pageSize = 10, List<Expression<Func<T, bool>>>? filters = null, params Expression<Func<T, object>>[] includes);
-        Task<T> GetByIdAsync(object id);
-        Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter, bool isTracked = false, params Expression<Func<T, object>>[] includes);
-        IQueryable<T> Queryable();
-        IQueryable<T> Where(Expression<Func<T, bool>> expression);
-    }
+    Task<List<T>> GetAllAsync(int page = 1, int pageSize = 10, List<Expression<Func<T, bool>>>? filters = null, params Expression<Func<T, object>>[] includes);
+    Task<T> GetByIdAsync(object id);
+    Task<IQueryable<T>> GetByFilterAsync(Expression<Func<T, bool>> filter, bool isTracked = false, params Expression<Func<T, object>>[] includes);
+    IQueryable<T> Where(Expression<Func<T, bool>> expression);
 }
--- src/Infrastructure/ShopAppAPI.Persistance/Repositories/ReadRepository.cs
+++ src/Infrastructure/ShopAppAPI.Persistance/Repositories/ReadRepository.cs
@@ -39,7 +39,7 @@ public class ReadRepository<T> : IReadRepository<T> where T : class, new()
         return result;
     }
 
-    public async Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter, bool isTracked = false, params Expression<Func<T, object>>[] includes)
+    public async Task<IQueryable<T>> GetByFilterAsync(Expression<Func<T, bool>> filter, bool isTracked = false, params Expression<Func<T, object>>[] includes)
     {
         var query = _context.Set<T>().AsQueryable();
 
@@ -49,11 +49,9 @@ public class ReadRepository<T> : IReadRepository<T> where T : class, new()
         if (includes != null)
             query = includes.Aggregate(query, (current, include) => current.Include(include));
 
-        return await query.FirstOrDefaultAsync(filter);
+        return query.Where(filter);
     }
     public async Task<T> GetByIdAsync(object id) => await _context.Set<T>().FindAsync(id);
-
-    public IQueryable<T> Queryable() => _context.Set<T>().AsQueryable();
 
     public IQueryable<T> Where(Expression<Func<T, bool>> expression)
         => _context.Set<T>().Where(expression).AsNoTracking();
